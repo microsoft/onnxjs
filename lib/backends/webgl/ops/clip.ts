ea32bbe96f6286f405bb70d0756d58b279e99a44
hasesh <hasesh@MININT-0I1G867>
2019-03-12 11:56:42 -0700

    Clip for WebGL

---
+++ lib/backends/webgl/ops/clip.ts
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import {Clip} from '../../../ops/clip';
+import {Tensor} from '../../../tensor';
+import {WebGLInferenceHandler} from '../inference-handler';
+import {ProgramInfo} from '../program-info';
+import {RunData} from '../program-manager';
+import {WebGLOperator} from '../webgl-operator';
+import {WebGLOperatorHelper} from '../webgl-operator-utils';
+
+export class WebGLClip extends Clip implements WebGLOperator {
+  run(inferenceHandler: WebGLInferenceHandler, inputs: Tensor[]): Tensor[] {
+    return WebGLOperatorHelper.run(this, inferenceHandler, inputs);
+  }
+  createProgramInfo(handler: WebGLInferenceHandler, inputs: Tensor[]): ProgramInfo {
+    const outputShape = inputs[0].dims.slice();
+    const shaderSource = `
+      const float min = float(${this.min});
+      const float max = float(${this.max});
+      uniform sampler2D A;
+      void main() {
+        float v = texture2D(A, TexCoords).r;
+        gl_FragColor = vec4(v < min ? min : (v > max) ? max : v);
+      }
+      `;
+    return {
+      hasMain: true,
+      inputLayouts: [handler.getOrCreateTextureLayout(inputs[0])],
+      outputLayout: handler.createBasicTextureLayout(outputShape),
+      shaderSource,
+    };
+  }
+  createRunData(handler: WebGLInferenceHandler, programInfo: ProgramInfo, inputs: Tensor[]): RunData {
+    const inputTDs = [handler.getOrCreate(inputs[0], programInfo.inputLayouts[0])];
+    return {
+      inputTextureDatas: inputTDs,
+      outputTextureData: handler.createTextureDataFromLayout(programInfo.outputLayout, inputTDs[0].dataType),
+      uniformData: {}
+    };
+  }
+}

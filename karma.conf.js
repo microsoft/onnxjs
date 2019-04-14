5498217bf1a90ecc71b05f545e44cbd807567f97
Yulong Wang <yulongw@microsoft.com>
2019-04-13 18:43:04 -0700

    test: update karma config for BrowserStack

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+const bundleMode = require('minimist')(process.argv)['bundle-mode'] || 'dev';  // 'dev'|'perf'|undefined;
+const mainFile = bundleMode === 'perf' ? 'test/onnx.perf.js' : 'test/onnx.dev.js';
+
+// it's a known issue that Safari does not work with "localhost" in BrowserStack:
+// https://www.browserstack.com/question/663
+//
+// we need to read machine IP address to replace "localhost":
+// https://stackoverflow.com/a/8440736
+//
+function getMachineIpAddress() {
+  var os = require('os');
+  var ifaces = os.networkInterfaces();
+
+  for (const ifname in ifaces) {
+    for (const iface of ifaces[ifname]) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        continue;
+      }
+
+      // returns the first available IP address
+      return iface.address;
+    }
+  }
+
+  // if no available IP address, fallback to "localhost".
+  return 'localhost';
+}
+
 module.exports = function(config) {
-  const bundleMode = require('minimist')(process.argv)['bundle-mode'] || 'dev';  // 'dev'|'perf'|undefined;
-
-  const mainFile = bundleMode === 'perf' ? 'test/onnx.perf.js' : 'test/onnx.dev.js';
-
   config.set({
     // global config of your BrowserStack account
     browserStack: {
@@ -27,64 +55,100 @@ module.exports = function(config) {
     },
     client: {captureConsole: true, mocha: {expose: ['body'], timeout: 60000}},
     preprocessors: {mainFile: ['sourcemap']},
-    reporters: ['mocha'],
-    browsers: [
-      'ChromeTest',
-      'ChromeDebug',
-      'Edge',
-      'Firefox',
-      'Electron',
-      'Safari',
-      'BS_WIN_Chrome',
-      'BS_WIN_Edge',
-      'BS_WIN_Firefox',
-      'BS_MAC_Chrome',
-      'BS_MAC_Safari',
-    ],
+    reporters: ['mocha', 'BrowserStack'],
+    browsers: [],
     captureTimeout: 120000,
     reportSlowerThan: 100,
     browserDisconnectTimeout: 600000,
     browserNoActivityTimeout: 300000,
     browserDisconnectTolerance: 0,
     browserSocketTimeout: 60000,
+    hostname: getMachineIpAddress(),
     customLaunchers: {
       ChromeTest: {base: 'Chrome', flags: ['--window-size=1,1']},
       ChromeDebug: {debug: true, base: 'Chrome', flags: ['--remote-debugging-port=9333']},
-      BS_WIN_Chrome: {
+      //
+      // ==== BrowserStack browsers ====
+      //
+
+      // Windows
+      //
+      BS_WIN_10_Chrome_73: {
         base: 'BrowserStack',
         browser: 'Chrome',
-        browser_version: '71.0',
+        browser_version: '73.0',
         os: 'Windows',
         os_version: '10',
       },
-      BS_WIN_Edge: {
+      BS_WIN_10_Edge_18: {
         base: 'BrowserStack',
         os: 'Windows',
         os_version: '10',
         browser: 'Edge',
         browser_version: '18.0',
       },
-      BS_WIN_Firefox: {
+      BS_WIN_10_Firefox_66: {
         base: 'BrowserStack',
         os: 'Windows',
         os_version: '10',
         browser: 'Firefox',
-        browser_version: '63.0',
+        browser_version: '66.0',
       },
-      BS_MAC_Chrome: {
+      BS_WIN_7_Chrome_63: {
         base: 'BrowserStack',
         browser: 'Chrome',
-        browser_version: '71.0',
-        os: 'OS X',
-        os_version: 'High Sierra',
+        browser_version: '63.0',
+        os: 'Windows',
+        os_version: '7',
       },
-      BS_MAC_Safari: {
+
+      // macOS
+      //
+      BS_MAC_10_14_Safari_12: {
+        base: 'BrowserStack',
+        os: 'OS X',
+        os_version: 'Mojave',
+        browser: 'Safari',
+        browser_version: '12.0',
+      },
+      BS_MAC_10_14_Chrome_73: {
+        base: 'BrowserStack',
+        os: 'OS X',
+        os_version: 'Mojave',
+        browser: 'Chrome',
+        browser_version: '73.0',
+      },
+      BS_MAC_10_13_Safari_11_1: {
         base: 'BrowserStack',
         os: 'OS X',
         os_version: 'High Sierra',
         browser: 'Safari',
         browser_version: '11.1',
-      }
+      },
+
+      // iPhone
+      //
+      BS_IOS_12_1_iPhoneXS: {
+        base: 'BrowserStack',
+        device: 'iPhone XS',
+        real_mobile: true,
+        os: 'ios',
+        os_version: '12.1',
+      },
+      BS_IOS_11_iPhoneX: {
+        base: 'BrowserStack',
+        device: 'iPhone X',
+        real_mobile: true,
+        os: 'ios',
+        os_version: '11',
+      },
+      BS_IOS_10_3_iPhone7: {
+        base: 'BrowserStack',
+        device: 'iPhone 7',
+        real_mobile: true,
+        os: 'ios',
+        os_version: '10.3',
+      },
     }
   });
 };

7760a099e34a850d956ce47da2427be5f4f2a0d3
Niels Garve <1102082+niels-garve@users.noreply.github.com>
2020-08-28 02:38:00 +0200

    webgl: fix broken module import caused by trailing `.` (#213)

--- lib/backends/webgl/glsl-registered-libs.ts
+++ lib/backends/webgl/glsl-registered-libs.ts
@@ -3,7 +3,7 @@
 
 import {CoordsGlslLib} from './glsl-coordinate-lib';
 import {GlslContext, GlslLib} from './glsl-definitions';
-import {EncodingGlslLib} from './glsl-encoding-lib.';
+import {EncodingGlslLib} from './glsl-encoding-lib';
 import {FragColorGlslLib} from './glsl-fragcolor-lib';
 import {ShapeUtilsGlslLib} from './glsl-shape-utils-lib';
 import {VecGlslLib} from './glsl-vec-lib';

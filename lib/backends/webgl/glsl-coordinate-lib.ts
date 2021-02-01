edd2c309ce93952fc189e238864dd29434e7f05d
Tixxx <tix@microsoft.com>
2021-02-01 15:34:38 -0800

    fixed condition check for packed inputs

--- lib/backends/webgl/glsl-coordinate-lib.ts
+++ lib/backends/webgl/glsl-coordinate-lib.ts
@@ -587,7 +587,7 @@ export class CoordsGlslLib extends GlslLib {
     this.context.programInfo.inputLayouts.forEach((inputLayout, i) => {
       const name = this.context.programInfo.samplers[i];
       const funcName = generateShaderFuncNameFromInputSamplerName(name);
-      if (this.context.programInfo.isInputsPacked) {
+      if (inputLayout.isPacked) {
         result[funcName] = this.getPackedSamplerFromInput(funcName, name, inputLayout);
       } else {
         result[funcName] = this.getUnpackedSamplerFromInput(funcName, name, inputLayout);
@@ -595,7 +595,7 @@ export class CoordsGlslLib extends GlslLib {
 
       const outCoordFuncName = generateShaderFuncNameFromInputSamplerNameAtOutCoords(name);
       if (inputLayout.unpackedShape.length <= outputLayout.unpackedShape.length) {
-        if (this.context.programInfo.isInputsPacked) {
+        if (inputLayout.isPacked) {
           result[outCoordFuncName] =
               this.getPackedSamplerAtOutputCoords(outCoordFuncName, inputLayout, outputLayout, name);
         } else {

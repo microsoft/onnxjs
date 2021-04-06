2ad812c60e944d887d338798b0de56d32b976509
xueyun zhu <xzhu1900@gmail.com>
2021-04-05 22:59:59 -0700

    clean up

--- lib/backends/backend-webgl.ts
+++ lib/backends/backend-webgl.ts
@@ -37,7 +37,6 @@ export class WebGLBackend implements Backend, WebGLOptions {
       if (typeof this.pack !== 'boolean') {
         this.pack = false;
       }
-      this.pack = true;
       Logger.verbose(
           'WebGLBackend',
           `Created WebGLContext: ${typeof this.glContext} with matmulMaxBatchSize: ${

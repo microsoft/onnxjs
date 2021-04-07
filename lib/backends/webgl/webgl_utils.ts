1a43fe03559bc0c9e598dec02d4358af7f06788f
Du Li <duli1@microsoft.com>
2021-04-07 13:40:24 -0700

    using reshape_packed kernel

--- lib/backends/webgl/webgl_utils.ts
+++ lib/backends/webgl/webgl_utils.ts
@@ -1,26 +1,12 @@
 import {Tensor} from '../../../lib/tensor';
 import {WebGLInferenceHandler} from './inference-handler';
-import {WebGLPack} from './ops/pack';
+import {WebGLReshapePacked} from './ops/reshape-packed';
 
-// this is un-optimized version of reshape: unpack + create_new_tensor + pack_with_new_shape
-// TODO: optimize it.
 export function reshape(inferenceHandler: WebGLInferenceHandler, input: Tensor, shape: number[]): Tensor {
-  // packing
-  const reshapedTensor = new Tensor(shape, input.type, undefined, undefined, input.data);
-  const key = `${shape}`;
-  let op = inferenceHandler.session.packOpCache.get(key);
-  if (!op) {
-    op = new WebGLPack();
-    inferenceHandler.session.packOpCache.set(key, op);
+  const op = new WebGLReshapePacked();
+  const newShape = new Tensor([shape.length], 'int32');
+  for (let i = 0; i < shape.length; i++) {
+    newShape.data[i] = shape[i];
   }
-  let artifact = inferenceHandler.session.programManager.getArtifact(op);
-  if (!artifact) {
-    const programInfo = op.createProgramInfo(inferenceHandler, [reshapedTensor]);
-
-    artifact = inferenceHandler.session.programManager.build(programInfo);
-    inferenceHandler.session.programManager.setArtifact(op, artifact);
-  }
-  const runData = op.createRunData(inferenceHandler, artifact.programInfo, [reshapedTensor]);
-  inferenceHandler.runProgram(artifact, runData);
-  return runData.outputTextureData.tensor;
+  return op.run(inferenceHandler, [input, newShape])[0];
 }
